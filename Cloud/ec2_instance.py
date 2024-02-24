c173a3a874c45ab8315307566243ca9f2ce5c639
larry <larryowusu99@gmail.com>
2024-02-24 23:20:23 +0000

    added new flow logic to ec2 deployment and created VPC

--- Cloud/ec2_instance.py
+++ Cloud/ec2_instance.py
@@ -15,30 +15,58 @@ instance_name = 'test_ec2'
 
 instance_id = None #this is a null value i.e it's either True or False
 
+# check if the instance already exists (via creating a list)
+
+instances = ec2.instances.all()
+instance_exist = False
+
+for instance in instances:
+    for tag in instance.tags:
+        if tag['Key'] == 'Name' and tag['Value'] == instance_name:
+            instance_exist = True
+            instance_id = instance.id
+            print(f"An instance named '{instance_name}' with id '{instance_id}' already exists.")
+            break
+    
+    if instance_exist:
+        break
+
+
 #ami: ami-052efd3df9dad4825
 
-# using the create instances method (function) to pass in these parameters down below
-new_instance = ec2.create_instances(
-    ImageId='ami-052efd3df9dad4825', #reqiured param
-    MinCount=1,
-    MaxCount=1,
-    InstanceType='t2.micro',
-    KeyName='boto3',# use the aws cli to create a key pair
-    TagSpecifications= [
-        {
-            'ResourceType':'instance',
-            'Tags': [{
-                 'Key': 'Name',
-                'Value': instance_name
+if not instance_exist:
+
+    # using the create instances method (function) to pass in these parameters down below
+    new_instance = ec2.create_instances(
+        ImageId='ami-052efd3df9dad4825', #reqiured param
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro',
+        KeyName='boto3',# use the aws cli to create a key pair
+        TagSpecifications= [
+            {
+                'ResourceType':'instance',
+                'Tags': [{
+                     'Key': 'Name',
+                    'Value': instance_name
+                },
+                ]
             },
-            ]
-        },
-    ]
-)
+        ]
+    )
+    instance_id = new_instance[0].id
+    print(f"An instance named '{instance_name}' with id '{instance_id}' created.")
 
 
-#stop an instance 
+
+#stop an instance via the stop() method
+ec2.Instance(instance_id).stop()
+print(f"An instance named '{instance_name}' with id '{instance_id}' has been stoped.")
 
 #start an instance
+ec2.Instance(instance_id).start()
+print(f"An instance named '{instance_name}' with id '{instance_id}' has been started.")
 
-#terminate an instance
+    #terminate an instance
+ec2.Instance(instance_id).terminate()
+print(f"An instance named '{instance_name}' with id '{instance_id}' has been terminated.")
